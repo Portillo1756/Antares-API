ed9e852c4a67c624cfc3e3d62880308314bb1754
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-07-04 15:54:18 -0400

    work on the js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,45 @@
-const fetchButton = document.getElementById('fetch-button')
+const cityInput = document.querySelector(".search-btn");
+const searchButton = document.querySelector(".search-btn");
 
-function getApi() {
-    const requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+const API_KEY = "";
 
-fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        for (let i = 0; i < data.lenght; i++) {
-            const listItem = document.createElement('li');
-            listItem.textContent = data[i].html_url;
-            repoList.appendChild(listItem);
-        }
-    });
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim(); //get city name and remove extra space
+    if(cityName) return; 
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    console.log(cityName);
 }
 
-fetchButton.addEventListener('click', getApi);
+searchButton.addEventListener("click", getCityCoordinates);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fetchButton = document.getElementById('fetch-button')
+
+// function getApi() {
+//     const requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+
+// fetch(requestUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         for (let i = 0; i < data.lenght; i++) {
+//             const listItem = document.createElement('li');
+//             listItem.textContent = data[i].html_url;
+//             repoList.appendChild(listItem);
+//         }
+//     });
+// }
+
+// fetchButton.addEventListener('click', getApi);
