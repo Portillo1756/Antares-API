dd67901b40cd0df8f9a75fb42e9059b3d03141af
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-07-04 23:22:40 -0400

    work on js file

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,55 @@
 const cityInput = document.querySelector(".search-btn");
 const searchButton = document.querySelector(".search-btn");
 
-const API_KEY = "";
+// API key for weatherstack
+const API_KEY = "c5fa2674d08fd03ad0df1e3ac2d4b508";
+
+const createWeatherCard = (weatherItem) => {
+    return ` <li class="card">
+            <h3>(${weatherItem.dt_txt.split("")[0]})</h3>
+            <img src="http://dataservice.accuweather.com/imagery/v1/maps/radsat/${weatherItem.weather[0].icon}" alt="weather-icon">
+            <h4>Temp: ${(weatherItem.main.temp - 2)}F</h4>
+            <h4>Wind: 4.31 M/S</h4>
+            <h4>Humidity: 82%</h4>
+            </li>`;
+} 
+
+const  getWeatherDetails = (cityName, lat, lon) => {
+    const WEATHER_API_URL = `http://dataservice.accuweather.com/forecasts/v1/daily/10day/{locationKey}`;
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        console.log(data);
+
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastData = new Data(forecast.dt_txt).getData();
+            if (!uniqueForecastDays.includes(forecastData)){
+               return uniqueForecastDays.push(forecastData);
+            }
+        });
+
+        console.log(fiveDaysForecast);
+        fiveDaysForecast.forEach(weatherItem => {
+            createWeatherCard(weatherItem);
+        });
+        }).catch(() => {
+        alert("an error occurred while fetching the weather forecast!");
+    });
+}
 
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); //get city name and remove extra space
-    if(cityName) return; 
+    if(!cityName) return; 
     const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
-    console.log(cityName);
+   
+    // get city coordinates (latitude, longitude, and name) from API
+   fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        if(!data.lenght) return alert(`no coordinates fpund for ${cityName}`);
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
+   }).catch(( => {
+          alert("an error occurred while fetching the coordinates!");
+   }));
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const fetchButton = document.getElementById('fetch-button')
-
-// function getApi() {
-//     const requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
-
-// fetch(requestUrl)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         for (let i = 0; i < data.lenght; i++) {
-//             const listItem = document.createElement('li');
-//             listItem.textContent = data[i].html_url;
-//             repoList.appendChild(listItem);
-//         }
-//     });
-// }
-
-// fetchButton.addEventListener('click', getApi);
