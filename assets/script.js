ade7d1a6337e4aa32755d78a176f259263781176
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-07-09 18:13:21 -0400

    project done

--- assets/script.js
+++ assets/script.js
@@ -16,13 +16,13 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
                 </div>
                 <div class="icon">
-                        <img src="http://dataservice.accuweather.com/imagery/v1/maps/radsat/${weatherItem.weather[0].icon}" alt="weather-icon">
+                        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
                         <h4>${weatherItem.weather[0].description}</h4>
                 </div>`;
     } else {
          return ` <li class="card">
-            <h3>(${weatherItem.dt_txt.split("")[0]})</h3>
-            <img src="http://dataservice.accuweather.com/imagery/v1/maps/radsat/${weatherItem.weather[0].icon}" alt="weather-icon">
+            <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
             <h4>Temp: ${(weatherItem.main.temp - 273.15)}F</h4>
             <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
             <h4>Humidity: ${weatherItem.main.humidity}%</h4>
@@ -34,33 +34,28 @@ const  getWeatherDetails = (cityName, lat, lon) => {
     const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-        console.log(data);
 
         const uniqueForecastDays = [];
         const fiveDaysForecast = data.list.filter(forecast => {
             const forecastData = new Date(forecast.dt_txt).getDate();
-            console.log(fiveDaysForecast)
             if (!uniqueForecastDays.includes(forecastData)) {
                return uniqueForecastDays.push(forecastData);
             }
         });
-
+    
             // earasing previous weather data
         cityInput.value = "";
         currentWeatherDiv.innerHTML = "";
         weatherCardsDiv.innerHTML = "";
         
-        
+
         fiveDaysForecast.forEach((weatherItem, index) => {
             if(index === 0) {
                 currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
             } else{
                 weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
             }
-        }).catch(() => {
-            alert("an error occurred while fetching the weather forecast!");
-        });
-
+        })
     });
 }
 
@@ -70,7 +65,7 @@ const  getWeatherDetails = (cityName, lat, lon) => {
             const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
                 // get city coordinates (latitude, longitude, and name) from API
             fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-            if(!data.lenght) return alert(`no coordinates found for ${cityName}`);
+            if(!data.length) return alert(`no coordinates found for ${cityName}`);
             const { name, lat, lon } = data[0];
             getWeatherDetails(name, lat, lon);
             }).catch(() => {
@@ -84,14 +79,11 @@ const  getWeatherDetails = (cityName, lat, lon) => {
 
             // get coordinates of user location
             const {latitude, longitude} = position.coords;
-            console.log(latitude)
-            console.log(longitude)
             const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
 
             // get city name from coordinates using reserve geocoding API
             fetch(REVERSE_GEOCODING_URL).then(res => res.json()).then(data => {
                 const {name} = data[0];
-                console.log(name)
                 getWeatherDetails(name, latitude, longitude);
            }).catch(() => {
                   alert("an error occurred while fetching the city!");
@@ -107,4 +99,3 @@ const  getWeatherDetails = (cityName, lat, lon) => {
 
 locationButton.addEventListener("click", getUserCoordinates);
 searchButton.addEventListener("click", getCityCoordinates);
-// cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates);
